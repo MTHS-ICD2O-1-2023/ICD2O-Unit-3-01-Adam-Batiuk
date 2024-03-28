a58fcc087c41ff434b85e599e08a9e12ceae036a
Adam-Batiuk <159185155+Adam-Batiuk@users.noreply.github.com>
2024-03-28 18:35:02 +0000

    math

--- js/script.js
+++ js/script.js
@@ -3,7 +3,16 @@
 // Created by: Adam Batiuk
 // Created on: Mar 2024
 // This file contains the JS functions for index.html
+"use strict"
 
 function myButtonClicked() {
-  alert('Hello, World!')
-}
+  // Define the lengths of the three sides of a shape
+  const side1 = parseInt(document.getElementById("side-1").value)
+  const side2 = parseInt(document.getElementById("side-2").value)
+  const side3 = parseInt(document.getElementById("side-3").value)
+
+  // alculate the area of the shape
+  const area = Math.sqrt((side1 + side2) / 2) * side3
+  const roundArea = Math.round(area * 100) / 100
+  
+  document.getElementById("answer").innerHTML = "Area is: " + roundArea + " mm²"
